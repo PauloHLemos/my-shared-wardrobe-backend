c8b66ee37822bc3e15d60e4dfc6add1126790b9d
Arunmolithevar, Thaarukan <thaarukan.arunmolithevar20@imperial.ac.uk>
2022-06-08 23:53:24 +0100

    feat: add hello request

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,16 @@ fn index() -> &'static str {
   "<Hello WORLD>"
 }
 
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: &str, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, we need to talk about your coolness.", name)
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index,hello])
 }
