5ed20f7135139272efb45a12fed1381c5afbbae3
Paulo Lemos <pl2420@ic.ac.uk>
2022-06-09 21:14:02 +0100

    feat: implement show_users function to retrieve users using diesel

---
+++ src/bin/show_users.rs
@@ -0,0 +1,21 @@
+extern crate drp02_backend;
+extern crate diesel;
+
+use self::drp02_backend::*;
+use self::models::*;
+use self::diesel::prelude::*;
+
+fn main() {
+    use drp02_backend::schema::Users::dsl::*;
+
+    let connection = establish_connection();
+    let results = Users
+        .limit(5)
+        .load::<User>(&connection)
+        .expect("Error loading users");
+
+    println!("Displaying {} users", results.len());
+    for user in results {
+        println!("{}", user.name);
+    }
+}
---
+++ src/models.rs
@@ -0,0 +1,5 @@
+#[derive(Queryable)]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+}
