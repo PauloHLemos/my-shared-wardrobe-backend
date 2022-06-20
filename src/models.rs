2b2229629e48bba32a4f6e0cb3269cfca2b31a5d
Paulo Lemos <pl2420@ic.ac.uk>
2022-06-20 11:30:10 +0100

    doc: add comments to models.rs

--- src/models.rs
+++ src/models.rs
@@ -7,30 +7,35 @@ use super::schema::{items, users, users_auth};
 // reference: https://medium.com/@james_32022/authentication-in-rocket-feb4f7223254
 // used tutorial to implement user authentication
 
+// extract info from users table
 #[derive(Queryable, Serialize)]
 pub struct User {
     pub uid: i64,
     pub name: String,
 }
 
+// extract info from users_auth table
 #[derive(Queryable, Serialize)]
 pub struct UserAuth {
     pub uid: i64,
     pub password_hash: String,
 }
 
+// data received to create new user
 #[derive(FromForm, Deserialize)]
 pub struct NewUserData<'a> {
     pub name: &'a str,
     pub password: &'a str,
 }
 
+// data passed to create new user in users table
 #[derive(Insertable, FromForm, Deserialize)]
 #[table_name="users"]
 pub struct NewUser<'a> {
     pub name: &'a str,
 }
 
+// data passed to users_auth table
 #[derive(Insertable, Deserialize)]
 #[table_name="users_auth"]
 pub struct NewUserAuth {
@@ -41,6 +46,7 @@ pub struct NewUserAuth {
 
 // ------------------------------------ item ------------------------------------------
 
+// extract info from items table
 #[derive(Queryable, Serialize, Debug)]
 pub struct Item {
     pub id: i64,
@@ -53,10 +59,10 @@ pub struct Item {
     pub likes: i64,
 }
 
+// data passed to create new item in items table
 #[derive(Insertable, Deserialize)]
 #[table_name="items"]
 pub struct NewItem<'a> {
-    // pub item_id: &'a i64,
     pub uid: i64,
     pub type_: &'a str,
     pub name: &'a str,
